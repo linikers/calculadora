4cc8b2d0c067c162b904c11eb21f9ef54a299be3
linikers <linikers@hotmail.com>
2022-10-27 17:15:17 -0300

    versão 0.06

--- dom.js
+++ dom.js
@@ -1,5 +1,15 @@
 const body = document.querySelector("body")
 
 const nav = document.createElement("nav")
+const ul = document.createElement("ul")
+const li1 = document.createElement("li")
+const li2 = document.createElement("li")
+const li3 = document.createElement("li")
+const li4 = document.createElement("li")
 
-body.appendChild(nav)
+
+body.append(nav, ul,li1, li2,li3,li4)
+
+const main = document.createElement("main")
+const section = document.createElement("section")
+body.append(main, section)
